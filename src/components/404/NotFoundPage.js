ff19a830194302e46243ceb3917c54902886128f
antyneskul <antyneskul@gmail.com>
2019-02-08 21:46:15 +0100

    added redux

--- src/components/404/NotFoundPage.js
+++ src/components/404/NotFoundPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-    <div>
+    <React.Fragment>
         404! - <Link to="/">Go Home</Link>
-    </div>
+    </React.Fragment>
 );
 
 
---
+++ src/containers/app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import AppRouter from '../../routes/AppRouter';
+
+const App = () => (
+    <AppRouter/>
+);
+
+
+export default App;
